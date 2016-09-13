33b34cb59743ad59f2ac8e1db70958de00dd988e
Codecool-Palko <matolaypal@gmail.com>
2016-09-13 09:59:06 +0200

    initila commit

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
-
 class Contact:
-    pass
+    all_contacts = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        cls.all_contacts = []
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def order(self, string):
+        Supplier.all_orders[self.email] = string
+
+
+class ContactList:
+
+    @staticmethod
+    def extendlist(list):
+        Contact.all_contacts.extend(list)
+
+    @staticmethod
+    def search(string):
+        return [item[0] for item in Contact.all_contacts if item[0] == string]
+
+    @staticmethod
+    def longest_name():
+        return max(Contact.all_contacts[0], key=len)
+
+a = Contact("pisti", "alma")
+b = Contact("zsoltiiiii", "narancs")
+c = Contact("pisti", "kelep")
+d = Contact("lali", "pokakip")
+
+# print(ContactList.search("pisti"))
+# print(ContactList.longest_name())
+print(Contact.all_contacts)
+
